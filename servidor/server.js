456f103c8411dbfe6401a10c3f021c34eb0fe503
lucas queiroz <lucasnotaroberto2@gmail.com>
2026-05-05 17:10:08 -0300

    yes

--- servidor/server.js
+++ servidor/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-app.use(express.static('./public/pag_inicial'));
+app.use(express.static('./public'));
 var server = http.createServer(app);
 server.listen(80);
 console.log("servidor rodando..")
